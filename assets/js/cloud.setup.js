0ec8160a3676e7d525f4b4999359e13f4b68eae2
isanavi7 <sinavarrov@gmail.com>
2018-10-01 20:35:33 -0500

    archivos assests

---
+++ assets/js/cloud.setup.js
@@ -0,0 +1,18 @@
+/**
+ * cloud.setup.js
+ *
+ * Configuration for this Sails app's generated browser SDK ("Cloud").
+ *
+ * Above all, the purpose of this file is to provide endpoint definitions,
+ * each of which corresponds with one particular route+action on the server.
+ *
+ * > This file was automatically generated.
+ * > (To regenerate, run `sails run rebuild-cloud-sdk`)
+ */
+/* eslint-disable */
+Cloud.setup({
+
+  methods: {"index":{"verb":"GET","url":"/","args":[]}}
+
+});
+/* eslint-enable */
