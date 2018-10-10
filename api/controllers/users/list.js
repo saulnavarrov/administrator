1e148a70efd2ccb70b5c9d59e2bbea36ef6ec3e0
isanavi7 <sinavarrov@gmail.com>
2018-10-10 00:19:56 -0500

    mejorando el codigo de respuesta de list usuarios arreglando la vista de usuarios y agregando nuevos metodos para la entrega de datos y arreglarlos en la pantalla poniendole una verificacion sencilla de usuarios al controlador para autorizar la vizualizacion de datos

--- api/controllers/users/list.js
+++ api/controllers/users/list.js
@@ -5,17 +5,74 @@ module.exports = {
 
   extendedDescription: ``,
 
-  inputs: {},
+  inputs: {
+    count: {
+      type:'boolean',
+      defaultsTo: false,
+      description: `Permitira hacer el conteo de todos los usuarios
+      este funcionara si esta activo y devolvera el numero de datos
+      encontrados`
+    },
+    lim: {
+      type: 'number',
+      defaultsTo: 10,
+      description: `Cantidad de resultados que entregara por cada
+      llamada que se realize al listado`
+    },
+    sk: {
+      type: 'number',
+      defaultsTo: 0,
+      description: `Omitira una cantidad de listados y visualizara en
+      en cantidades por limites, usado para paginar la cantidad de resultados
+      que se entregan en la vista`
+    }
+  },
 
-  exits: {},
+  exits: {
+    success: {
+      description: 'Entrega de usuarios Exitosa.'
+    },
+
+    notFound: {
+      responseType: 'notFound',
+      description: 'Datos no encontrados de los usuario o no existe ninguno'
+    },
+
+    unauthorized: {
+      statusCode: 401,
+      responseType: 'unauthorized',
+      description: 'No autorizado para ver los resultados de la pagina'
+    }
+  },
 
   fn: async function (inputs, exits) {
 
     let rq = this.req;
-    let usersArray = await User.find(); // Todos los usuarios
     let users = []; // array de usuario nuevo
-    // let userId = rq.session.userId;
-    // let isSocket = rq.isSocket;
+    let userId = rq.session.userId;
+    let isSocket = rq.isSocket;
+
+    // Verificacion de usuario
+    if(!userId && !isSocket){
+      return exits.unauthorized({
+        error: true,
+        message: 'Unauthorized'
+      });
+    }
+
+    // Enviando Numero complto de registros que hay en la base de datos
+    if(inputs.count){
+      return exits.success({
+        model: 'users',
+        count: await User.count()
+      });
+    }
+
+    // Ejecucion luego entregar el contador y saber que esta autorizado
+    // para visualizar estos datos
+    let usersArray = await User.find()
+                            .limit(inputs.lim)
+                            .skip(inputs.lim * inputs.sk); // Todos los usuarios
 
     // Protegiendo el Password para no visualizarlo en Json
     for (user of usersArray) {
--- assets/js/pages/users/list.page.js
+++ assets/js/pages/users/list.page.js
@@ -4,23 +4,115 @@ parasails.registerPage('list', {
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
   data: {
     //…
+    c: 1,
+    listData: {},
+    listCount: 0,
+    listCountComplete: 0,
+    h: 'Data Null',
+
+    progressBar: true,
+    tableData: false,
+    footerTable: false,
+    search: false,
+    countData: false,
+    navegationsData: false,
+    numResult: false,
+
+    alert: {
+      active: false,
+      icon: 'ion-ios-information-outline',
+      title: 'Titulo de la alerta',
+      message: 'Mensaje de la alerta content',
+      type: 'alert-info'
+    }
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
   //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
   //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
-  beforeMount: function() {
+  beforeMount: async function() {
     // Attach any initial data from the server.
     _.extend(this, SAILS_LOCALS);
+
+    // trae toda la base de datos de usuarios
+    await this.dataDb();
+    await this.countComplete();
+
   },
   mounted: async function() {
     //…
+    console.log(this.listData);
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
   //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
+
     //…
+    dataDb: async function () {
+      let csrfToken = window.SAILS_LOCALS._csrf;
+
+      await io.socket.request({
+        url: '/api/v1/users',
+        method: 'POST',
+        data: {},
+        headers: {
+          'content-type': 'application/json',
+          'x-csrf-token': csrfToken
+        }
+      }, (resData, jwres) => {
+        // En caso de error
+        if (jwres.error) {
+          this.progressBar = false;
+          if (jwres.statusCode >= 500 && jwres.statusCode <= 599) {
+            this.alert = {
+              active: true,
+              type: 'alert-danger',
+              icon: 'ion-ios-close-outline',
+              title: `Error: ${jwres.statusCode} - ${jwres.body}`,
+              message: jwres.error.message
+            };
+          } else if (jwres.statusCode >= 400 && jwres.statusCode <= 499){
+            this.alert = {
+              active: true,
+              type: 'alert-warning',
+              icon: 'ion-ios-close-outline',
+              title: `Error: ${jwres.statusCode} - ${jwres.body}`,
+              message: jwres.error.message
+            };
+          }
+        }
+
+        if(jwres.statusCode === 200) {
+          this.progressBar = false;
+          this.tableData = this.search = this.footerTable = this.countData = true;
+          this.listCount = resData.list.length;
+          this.listData = resData.list;
+        }
+      });
+    },
+
+    countComplete: async function() {
+      let csrfToken = window.SAILS_LOCALS._csrf;
+      await io.socket.request({
+        url: '/api/v1/users',
+        method: 'POST',
+        data: {
+          count: true
+        },
+        headers: {
+          'content-type': 'application/json',
+          'x-csrf-token': csrfToken
+        }
+      }, (resData, jwres) => {
+        if (jwres.statusCode === 200) {
+          if(resData.count > this.listCount){
+            this.navegationsData = this.numResult = true;
+          }
+          this.listCountComplete = resData.count;
+        }
+      });
+    }
   }
 });
