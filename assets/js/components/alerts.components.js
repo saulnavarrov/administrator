5ccdfaf970cb0fa3d11678f1491bf88290ecc2fb
Saul Navarrov <sinavarrov@gmail.com>
2019-01-29 23:04:29 -0500

    Componet Alert Vue.js

---
+++ assets/js/components/alerts.components.js
@@ -0,0 +1,76 @@
+/**
+ * <alerts>
+ */
+
+
+parasails.registerComponent('alerts', {
+  //  ╔═╗╦═╗╔═╗╔═╗╔═╗
+  //  ╠═╝╠╦╝║ ║╠═╝╚═╗
+  //  ╩  ╩╚═╚═╝╩  ╚═╝
+  props: [
+    'active',
+    'icon',
+    'message',
+    'title',
+    'type',
+  ],
+
+
+  //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+  //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+  //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+  data: function () {
+    return {
+      //…
+    };
+  },
+
+
+  //  ╦ ╦╔╦╗╔╦╗╦
+  //  ╠═╣ ║ ║║║║
+  //  ╩ ╩ ╩ ╩ ╩╩═╝
+  template: `
+  <div class="row" v-show="active">
+    <div class="col-12">
+      <div class="alert alert-has-icon alert-dismissible show fade " :class="[type]" >
+        <div class="alert-icon">
+          <i class="ion" :class="[icon]"></i>
+        </div>
+        <div class="alert-body">
+          <button class="close" data-dismiss="alert">
+            <span>×</span>
+          </button>
+          <div class="alert-title">{{ title }}</div>
+          {{ message }}
+        </div>
+      </div>
+    </div>
+    <br>
+  </div>
+  `,
+
+
+  //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+  //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+  //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+  beforeMount: function() {
+    //…
+  },
+  mounted: async function(){
+    //…
+  },
+  beforeDestroy: function() {
+    //…
+  },
+
+  //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+  //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+  //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+  methods: {
+    prueba: async function () {
+
+
+      this.$pruebas();
+    },
+  }
+});
