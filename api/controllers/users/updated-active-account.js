8009cc0f1a6bb1cb19591f4858ad551c01c3d26b
Saul Navarrov <sinavarrov@gmail.com>
2019-02-04 18:44:14 -0500

    Nuevas acciones para users

---
+++ api/controllers/users/updated-active-account.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'Updated active account',
+
+
+  description: '',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    return exits.success();
+
+  }
+
+
+};
---
+++ api/controllers/users/updated-reconfirm-email.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'Updated reconfirm email',
+
+
+  description: '',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    return exits.success();
+
+  }
+
+
+};
