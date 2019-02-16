4510ca35d1a58e2fa86c758cf22cb25c6280622d
Saul Navarrov <sinavarrov@gmail.com>
2019-02-16 11:19:56 -0500

    archivo de buscador de empresas

---
+++ api/controllers/masters/holding/search.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'Search',
+
+
+  description: 'Search holding.',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    return exits.success();
+
+  }
+
+
+};
