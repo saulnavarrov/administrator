61d82c1e52b6ef22f04333bf2d047b06104ea119
Saul Navarrov <sinavarrov@gmail.com>
2019-04-08 21:46:02 -0500

    para cambiar contraseña al usuario desde el administrador

---
+++ api/controllers/users/updated-change-password.js
@@ -0,0 +1,25 @@
+/**
+ * updated-change-password.js
+ *
+ * @description :: Mirar abajo la description ► ↓↓↓
+ *
+ * @src {{proyect}}/api/controllers/users/updated-active-account.js
+ * @author Saul Navarrov <Sinavarrov@gmail.com>
+ * @created 2019/04/08
+ * @version 1.0
+ */
+module.exports = {
+  friendlyName: 'Updated Change Passwords',
+
+  description: '',
+
+  extendedDescription: ``,
+
+  inputs: {},
+
+  exits: {},
+
+  fn: async function (inputs, exits) {
+
+  }
+};
