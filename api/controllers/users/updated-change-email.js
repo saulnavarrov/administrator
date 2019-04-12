c664fe684de9bad801b4d5ef250a880947162f9d
Saul Navarrov <sinavarrov@gmail.com>
2019-04-12 12:48:28 -0500

    b-change email complete
    el backend terminado con todas sus funciones listas

--- api/controllers/users/updated-change-email.js
+++ api/controllers/users/updated-change-email.js
@@ -21,7 +21,18 @@ module.exports = {
       type: 'string',
       defaultsTo: '',
       description: `buscara el usuario con la id`
+    },
+    newEmail: {
+      type: 'string',
+      defaultsTo: '',
+      description: `Correo electronico por el cual se va hacer el cambio.`,
+    },
+    confirmNewEmail: {
+      type: 'string',
+      defaultsTo: '',
+      description: `Confirmacion del correo electronico por el que se va hacer el cambio`
     }
+
   },
 
   exits: {
@@ -36,6 +47,11 @@ module.exports = {
       statusCode: 401,
       responseType: 'unauthorized',
       description: 'No autorizado para ver los resultados de la pagina'
+    },
+    badRequest: {
+      statusCode: 400,
+      responseType: 'badRequest',
+      description: 'Error comun que sucede'
     }
   },
 
@@ -45,9 +61,11 @@ module.exports = {
      ***************************************************************************************/
     const rq = this.req;
     const _ = require('lodash');
+    const moment = require('moment');
     const userId = rq.session.userId;
     const isSocket = rq.isSocket;
     const updatedAt = moment().toJSON();
+    var valEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     let count = 0;
 
     // Configurando Moment
@@ -108,10 +126,80 @@ module.exports = {
       });
     }
 
+    // Verificando emails vacios
+    if (inputs.newEmail.length === 0 || inputs.confirmNewEmail.length === 0 || typeof(inputs.newEmail) === 'undefined' || typeof(inputs.confirmNewEmail) === 'undefined') {
+      return exits.badRequest({
+        error: true,
+        code: 'error_data_incomplete',
+        message: `Los campos necesarios para realizar esta accion esta incompleta`
+      });
+    }
+
+    // Verificando si son emails
+    if (!valEmail.test(inputs.newEmail) || !valEmail.test(inputs.confirmNewEmail)) {
+      return exits.badRequest({
+        error: true,
+        code: 'error_data_incomplete',
+        message: `Los campos necesarios para realizar esta accion esta incompleta`
+      });
+    }
+
+    // Coincidencia de los emails
+    if (inputs.newEmail !== inputs.confirmNewEmail) {
+      return exits.badRequest({
+        error: true,
+        code: 'error_data_incomplete',
+        message: `Los campos necesarios para realizar esta accion esta incompleta`
+      });
+    }
+
 
     /***************************************************************************************
      * BLOQUE DE TRABAJO
      ***************************************************************************************/
+    let newEmail = inputs.newEmail;
+
+    // Verificando el correo electronico no coincida con otro
+    let userb = await Users.findOne({emailAddress: newEmail}).select(['id','emailAddress']);
+
+    // Devolviendo error de correo existente
+    if (userb){
+      let mssBad = userb.id === inputs.id ? 'El correo electronico es igual al anterior, intenta con otro' : false;
+      return exits.badRequest({
+        error: true,
+        code: 'error_email_exists',
+        message: mssBad ? mssBad : `Ingrese otro correo electronico diferente: Este correo ya existe.`
+      });
+    }
+
+    // Buscando el usuario a quien se le va hacer el cambio
+    let userc = await Users.findOne({id:inputs.id}).select(['id','name','lastName']);
+    let emailProofToken = await sails.helpers.strings.random('url-friendly');
+    let emailProofTokenExpiresAt = Date.now() + sails.config.custom.emailProofTokenTTL;
+
+    // Ingresando el email candidato para hacer el cambio de emails
+    await Users.update({
+      id: inputs.id
+    })
+    .set({
+      emailChangeCandidate: newEmail,
+      emailProofToken: emailProofToken,
+      emailProofTokenExpiresAt: emailProofTokenExpiresAt,
+      emailStatus: 'changeRequested',
+      updatedAt: updatedAt
+    });
+
+    // Enviando correo electronico al nuevo email
+    await sails.helpers.sendTemplateEmail.with({
+      to: newEmail,
+      subject: 'Confirmar el Cambio de Correo Electronico',
+      template: 'email-verify-new-email',
+      templateData: {
+        fullName: `${userc.name} ${userc.lastName}`,
+        token: emailProofToken,
+        expira: moment(emailProofTokenExpiresAt).format('LLLL')
+      }
+    });
 
     return exits.success();
   }
