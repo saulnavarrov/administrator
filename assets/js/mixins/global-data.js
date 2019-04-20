ee75febae68e7780b07470e8c541e909d85e6f8f
Saul Navarrov <sinavarrov@gmail.com>
2019-04-20 09:56:35 -0500

    data global mixins

---
+++ assets/js/mixins/global-data.js
@@ -0,0 +1,43 @@
+/**
+ * global-data.js
+ *
+ * @description :: Mixins de datos de manera global y que se use
+ *  en todos los modulos maestros.
+ * @src {{proyect}}/assets/js/mixins/global-data.js
+ * @author Saul Navarrov <Sinavarrov@gmail.com>
+ * @created 2019/04/20
+ * @version 1.0
+ */
+
+var globalData = {
+  //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+  //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+  //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+  data: function data() {
+    return {
+      init: 'Datos Globales'
+    };
+  },
+
+  //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+  //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+  //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+  created: function () {
+    //…
+  },
+  beforeMount: function() {
+    //…
+  },
+  mounted: async function(){
+    //…
+  },
+  beforeDestroy: function() {
+    //…
+  },
+
+  //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+  //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+  //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+  methods: {
+  }
+};
